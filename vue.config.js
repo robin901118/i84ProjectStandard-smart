96d6ed9ae6f5c776c25986856365b36c1d2b62aa
robin901118 <303278055@qq.com>
2020-08-11 14:25:37 +0800

    修复本地开发环境跨域的问题

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const baseUrl = process.env.NODE_ENV === 'production' ? './' : '/'
 
 
 const devServe = ()=>{
-  if(process.env.VUE_APP_CURRENTMODE === "mock" || process.env.VUE_APP_CURRENTMODE === "test"){
+  if(process.env.VUE_APP_CURRENTMODE === "mock" || process.env.VUE_APP_CURRENTMODE === "serve"){
     return {
       port: 9090,
       proxy: {
